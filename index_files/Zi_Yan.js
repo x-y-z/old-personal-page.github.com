8e11e8a75a21a16271fbb638d8c72aeaec6e0d72
Zi Yan <birdmanyan@gmail.com>
2011-11-21 16:45:12 -0500

    First commit

---
+++ index_files/Zi_Yan.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20110901
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Zi_Yan_files/Zi_YanMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
